c779109da076e13d6ac4c4e4c98f535d00ac4332
kingbowx <kfengdev@gmail.com>
2012-07-26 13:00:20 +1000

    dynamic pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should contain Home" do
+      get 'home'
+      response.should have_selector("title", :content => "Sample App | Home")
+    end
+
+    it "should contain head text" do
+      get 'home'
+      response.should contain ('head text')
     end
   end
 
