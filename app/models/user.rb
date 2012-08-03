0278582fb2b00d034aa1f9f0fb9e0b6253146289
kingbowx <kfengdev@gmail.com>
2012-08-03 17:21:07 +1000

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
   
-  validates :name, :presence => true
+  validates :name,  :presence => true,
+                    :length => { :maximum => 50 }
+  
+  email_regex = /\A[\w+.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false}
+                    
 end
